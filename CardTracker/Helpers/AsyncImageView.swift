4bdebbf9bd7726d92682330c3d820900451b09d8
Michael Rowe <support@theapapp.com>
2024-03-23 13:07:30 -0400

    Corrected Spelling error and attempting to address performance with AsyncImage

---
+++ CardTracker/Helpers/AsyncImageView.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncImageView.swift
+//  Greet Keeper
+//
+//  Created by Michael Rowe on 3/23/24.
+//
+
+import SwiftUI
+
+struct AsyncImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncImageView()
+}
---
+++ CardTracker/Helpers/ImageLoader.swift
@@ -0,0 +1,8 @@
+//
+//  ImageLoader.swift
+//  Greet Keeper
+//
+//  Created by Michael Rowe on 3/23/24.
+//
+
+import Foundation
