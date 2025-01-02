9b74b18f95b56ad4e5f30a28e3602721d28728c6
Michael Rowe <support@theapapp.com>
2025-01-01 19:53:28 -0500

    Downgrading to iOS17.X for additional testers.. working to create edit features on Menu

---
+++ CardTracker/ScreenViewTestAnother.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenViewTestAnother.swift
+//  Greet Keeper
+//
+//  Created by Michael Rowe1 on 1/1/25.
+//
+
+import SwiftUI
+
+struct ScreenViewTestAnother: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenViewTestAnother()
+}
