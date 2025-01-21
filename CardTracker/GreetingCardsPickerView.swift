9ca4ae787cf3b59e439bb656b61222ed87833b50
Michael Rowe <support@theapapp.com>
2025-01-21 10:15:35 -0500

    More xcode/git conflict clean up

--- CardTracker/GreetingCardsPickerView.swift
+++ CardTracker/GreetingCardsPickerView.swift
@@ -5,13 +5,6 @@
 //  Created by Michael Rowe on 12/19/23.
 //
 
-//
-//  GreetingCardsPicker.swift
-//  CardTracker
-//
-//  Created by Michael Rowe on 12/19/23.
-//
-
 import os
 import SwiftData
 import SwiftUI
@@ -19,20 +12,13 @@ import SwiftUI
 struct GreetingCardsPickerView: View {
     @Environment(\.modelContext) var modelContext
     @Environment(\.presentationMode) var presentationMode
-
-    // Added State for Search Text
-    @State private var searchText = ""
-
+    
     @Query(sort: \GreetingCard.cardName) private var greetingCards: [GreetingCard]
     
     @Binding var selectedGreetingCard: GreetingCard?
-<<<<<<< Updated upstream
     
     @State private var searchText = ""
     
-=======
-
->>>>>>> Stashed changes
     private var gridLayout = [
         GridItem(.adaptive(minimum: 180), spacing: 5, alignment: .center)
     ]
@@ -50,17 +36,11 @@ struct GreetingCardsPickerView: View {
             ]
         )
     }
-<<<<<<< Updated upstream
     
-=======
-
-    // Computed property to filter the cards based on searchText
->>>>>>> Stashed changes
     var filteredGreetingCards: [GreetingCard] {
         if searchText.isEmpty {
             return greetingCards
         } else {
-<<<<<<< Updated upstream
             return greetingCards.filter { card in
                 card.cardName.localizedCaseInsensitiveContains(searchText)
             }
@@ -71,26 +51,11 @@ struct GreetingCardsPickerView: View {
             ScrollView {
                 LazyVGrid(columns: gridLayout, alignment: .center, spacing: 5) {
                     ForEach(filteredGreetingCards, id: \.id) { greetingCard in
-=======
-            return greetingCards.filter { $0.cardName.localizedCaseInsensitiveContains(searchText) }
-        }
-    }
-
-    var body: some View {
-        NavigationView { // Added NavigationView to handle search bar
-            ScrollView {
-                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 5) {
-                    ForEach(filteredGreetingCards, id: \.id) { greetingCard in // Using filtered array
->>>>>>> Stashed changes
                         VStack {
                             Image(uiImage: UIImage(data: (greetingCard.cardFront)!) ?? UIImage(named: "frontImage")!)
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
-<<<<<<< Updated upstream
                                 .frame(width: 175, height: 175)
-=======
-                                .frame(width: 195, height: 195)
->>>>>>> Stashed changes
                                 .clipShape(RoundedRectangle(cornerRadius: 5))
                                 .onTapGesture {
                                     selectedGreetingCard = greetingCard
@@ -101,7 +66,6 @@ struct GreetingCardsPickerView: View {
                         }
                     }
                 }
-<<<<<<< Updated upstream
                 .padding(.horizontal)  // Reduce horizontal padding
                 .padding(.top, -5)
             }
@@ -111,18 +75,6 @@ struct GreetingCardsPickerView: View {
                 Text("Select \(eventType.eventName) Card")
                         .foregroundColor(Color.accentColor)
                 }
-=======
-                .padding()
-            }
-            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search Cards") // search bar added
-            .navigationTitle("Select \(eventType.eventName) Card")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-        .toolbar {
-            ToolbarItem(placement: .principal) {
-                Text("Select \(eventType.eventName) Card")
-                    .foregroundColor(Color.accentColor)
->>>>>>> Stashed changes
             }
     }
 }
